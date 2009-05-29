5491215436f004cf00ee2addca393bf8bb99b686
G <githappenedhere@gmail.com>
2009-05-29 15:31:06 -0700

    make secret cookie tokens harder to guess

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ class MainHandler(webapp.RequestHandler):
 
     def post(self):
         bin = Bin()
-        bin.privatebin = self.make_secret_maybe()
+        bin.privatebin = self.make_secret_maybe( bin )
         bin.escapehtml = bool( self.request.get( 'escapehtml' ) )
         self.response.headers.add_header( 'Set-Cookie', 'pb_' + bin.name + '=' + bin.privatebin )
         self.response.headers.add_header( 'Cache-Control', 'no-cache' ) # FIX: attempt to avoid cache bug? http://code.google.com/p/googleappengine/issues/detail?id=732
@@ -25,14 +25,17 @@ class MainHandler(webapp.RequestHandler):
         bin.put()
         self.redirect('/%s' % bin.name)
     
-    def make_secret_maybe( self ):
+    def make_secret_maybe( self, bin ):
         retval = ''
         if bool( self.request.get( 'privatebin' ) ):
+            retval += bin.name
             secret = md5.new()
             secret.update( 'postbin ' + os.urandom( 42 ) )
             secret.update( secret.hexdigest() )
-            secret.update( 'postbin ' + os.urandom( 42 ) )
-            retval = secret.hexdigest()
+            retval += '_' + secret.hexdigest()
+            secret.update( 'postbin ' + os.urandom( 56 ) )
+            secret.update( secret.hexdigest() )
+            retval += '_' + secret.hexdigest()
         return retval
 
 
